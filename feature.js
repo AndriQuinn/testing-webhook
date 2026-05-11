34c613ec4f9f5eb16444ccbdd62600c8007fc654
Andri Quinn <dnlndrgntqntcn@gmail.com>
2026-05-11 21:21:47 +0800

    limit check?

--- feature.js
+++ feature.js
@@ -1,2 +1,4 @@
 console.log("testing ratelimit in webhook")
 console.log("another testing ratelimit in webhook currently in 4/5")
+console.log("another testing ratelimit in webhook currently in 5/5")
+console.log("this should stopped the req because of ratelimit")
