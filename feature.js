6bd8e3c2ee2274911789212ea61e119d04fea43a
Andri Quinn <dnlndrgntqntcn@gmail.com>
2026-05-11 18:17:42 +0800

    Greet?

--- feature.js
+++ feature.js
@@ -1,6 +1,6 @@
 // feature.js
-function greet(name) {
-    return `Hello there, ${name}`
+function greetShoa(name) {
+    return `Hi Im Shoa, Im here to review your pull request ${name}`
 }
 
-console.log(greet('Shoa'))
+console.log(greetShoa('Andri'))
