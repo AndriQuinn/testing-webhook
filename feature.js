f91e2c2fe3bd425e04e19f324aaae8e12587f437
Andri Quinn <dnlndrgntqntcn@gmail.com>
2026-05-11 21:19:49 +0800

    testing

--- feature.js
+++ feature.js
@@ -1,13 +1,1 @@
-function shoaGreeting(name) {
-    return `Hi Im Shoa, Im gonna have to review your code ${name}`
-}
-
-function exponentiate(num,pow) {
-    let result = Infinity
-    for (let i = 1; i <= pow; i++) {
-        result *= num * num
-    }
-    return result
-}
-
-console.log(shoaGreeting('Andri'))
+console.log("testing ratelimit in webhook")
