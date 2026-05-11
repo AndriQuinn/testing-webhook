8a9c00fdac007f676e6c0d5699f59c364aee1b58
Andri Quinn <dnlndrgntqntcn@gmail.com>
2026-05-11 21:14:04 +0800

    expo?

--- feature.js
+++ feature.js
@@ -2,4 +2,12 @@ function shoaGreeting(name) {
     return `Hi Im Shoa, Im gonna have to review your code ${name}`
 }
 
+function exponentiate(num,pow) {
+    let result = Infinity
+    for (let i = 1; i <= pow; i++) {
+        result *= num * num
+    }
+    return result
+}
+
 console.log(shoaGreeting('Andri'))
