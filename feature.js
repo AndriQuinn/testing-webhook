f9c6d668f4b1bc2b484d47d5c67526d5c71cb7a2
Andri Quinn <dnlndrgntqntcn@gmail.com>
2026-05-12 10:31:35 +0800

    correct exponentiate3?

--- feature.js
+++ feature.js
@@ -1,11 +1,7 @@
-function exponentiaate(num,exp) {
-    let result = num
-    for (let i = i; i <= Math.floor(exp); i++) {
-        result *= num
+function exponentiate(num, exp) {
+    let result = 1;
+    for (let i = 0; i < Math.floor(exp); i++) {
+        result *= num;
     }
-
-    return result
+    return result;
 }
-
-console.log(exponentiaate(2,5))
-console.log(exponentiaate(5,5))
