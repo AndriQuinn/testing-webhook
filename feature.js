9a446add92f7b2d67eea28bb7e3f877d7a414df7
Andri Quinn <dnlndrgntqntcn@gmail.com>
2026-05-12 08:04:36 +0800

    RL #2

--- feature.js
+++ feature.js
@@ -1,6 +1,7 @@
 // Rate Limiter Testing
 // Just modified the webhook to make the rate limiter comes first before anything else
 
-// Counter 1?
+// Counter 2?
 
 // If you review this kindly ignore this :)
+console.log("Counter 2")
